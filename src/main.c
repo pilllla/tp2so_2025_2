37814906aad1b93b1afffd9ac23a3236e93c2e9b
Carolina Pilla <carol.moyapilla@gmail.com>
2025-11-07 17:41:49 -0300

    init repo

---
+++ src/main.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+
+/**
+ * criar a tabela das paginas (pode ser multinivel)
+ * ler um arquivo deve pegar o endereco e a opcao de escrita ou leitura
+ *
+ * entrada:
+ *  arquivo
+ *  tamanho da pagina: kb
+ *  tamanho da mem física: kb
+ */
+
+void print_info()
+{
+}
+
+int main(int argc, char **argv)
+{
+    unsigned int pg_s, mem_sz;
+    unsigned int addr;
+    char rw;
+
+    if (argc < 4)
+    {
+        perror("Argumentos faltando: ");
+    }
+
+    pg_s = atoi(argv[2]);
+    mem_sz = atoi(argv[3]);
+
+    FILE *fp;
+    fp = fopen(argv[1], "r");
+
+    if (fp == 0)
+    {
+        perror("erro ao abrir o aquivo");
+    }
+
+    while (fscanf(fp, "%x %c",addr, rw)==2){
+
+    }
+
+        return 0;
+}
